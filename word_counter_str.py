bc1ef50d999954e97410c871a7f89a7f6929fadc
dhashnamoorthipalanivel <dhashnamoorthipalanivel@gmail.com>
2025-04-07 22:25:11 +0530

    word counter program in string manipulation

---
+++ word_counter_str.py
@@ -0,0 +1,11 @@
+# Word Counter
+# Ask for a sentence input.
+
+# Count and print the number of words.
+
+mySentence = "Hello everyone, welcome to the python world"
+sentenceSplit = mySentence.split()
+count = 0
+for text in sentenceSplit:
+    count+=1
+print("Total number of words in string: ",count)
